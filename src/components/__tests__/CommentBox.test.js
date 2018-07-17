425ae520420bbd4b677537cc1eebcbce2b3ae8ef
Brian Bycynski <high5bri@hotmail.com>
2018-07-17 08:43:25 -0400

    finished lecture 40

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -17,12 +17,23 @@ it('Has a text area and a button', () => {
     expect(wrapped.find('button').length).toEqual(1);
 });
 
-it('Has a text area to type in', () => {
-    wrapped.find('textarea').simulate('change', {
-        target: {
-            value: 'new comment'
-        }
+describe('The text area', () => {
+    beforeEach(() => {
+        wrapped.find('textarea').simulate('change', {
+            target: {
+                value: 'new comment'
+            }
+        });
+        wrapped.update();
     });
-    wrapped.update();
-    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+
+    it('Has a text area to type in', () => {
+        expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+    });
+
+    it('when form is submitted, text area gets emptied', () => {
+        wrapped.find('form').simulate('submit');
+        wrapped.update();
+        expect(wrapped.find('textarea').prop('value')).toEqual('');
+    });
 });
